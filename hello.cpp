64f62885113bc201dbbca756361892997ce41fab
Russell Yanofsky <russ@yanofsky.org>
2002-06-18 03:01:40 +0000

    - removed unused Base class. - no other changes aside from rearranging the order in which some classes are   declared.

--- hello.cpp
+++ hello.cpp
@@ -41,97 +41,6 @@ struct TypeList
   typedef TAIL Tail;
 };
 
-/////////////////////////////////////////////////////////////////////////////
-// Testing Stuff
-
-template<class ACTUAL, class BASE>
-struct UpperPolicyImpl : public BASE
-{
-  void upperGo()
-  {
-    ACTUAL * t = (ACTUAL *) this;
-    t->UpperMethod();
-    t->LowerType::shadow();
-  }
-  
-  void shadow()
-  {
-    cout << "Upper Shadow" << endl;
-  }
-  
-  void crawl()
-  {
-    typedef BASE Parent;
-    cout << __FILE__ << ":" << __LINE__ << " " << typeid(Parent).name() << endl;
-    Parent::crawl();
-  }  
-};
-
-struct UpperPolicy
-{
-  typedef UpperPolicy Identity;
-  typedef EmptyType Params;
-};
-
-template<class ACTUAL, class BASE>
-struct LowerPolicyImpl : public BASE
-{
-  void lowerGo()
-  {
-    ACTUAL * t = (ACTUAL *) this;
-    cout << "Lower: My BASE is " << typeid(BASE).name() << endl;
-    t->LowerMethod();
-  }
-  
-  void shadow()
-  {
-    cout << "Lower Shadow" << endl;
-  }
-  
-  void crawl56()
-  {
-    typedef BASE Parent;
-    cout << __FILE__ << ":" << __LINE__ << " " << typeid(Parent).name() << endl;
-    Parent::crawl();
-  }
-};
-
-struct LowerPolicy
-{
-  typedef LowerPolicy Identity;
-  typedef EmptyType Params;
-};
-
-template<>
-struct Specialize<UpperPolicy>
-{
-  template<class PARAMS, class ACTUAL, class BASE>
-  struct Params
-  {
-    typedef UpperPolicyImpl<ACTUAL, BASE> Result;
-  };
-};
-
-template<>
-struct Specialize<LowerPolicy>
-{
-  template<class PARAMS, class ACTUAL, class BASE>
-  struct Params
-  {
-    typedef LowerPolicyImpl<ACTUAL, BASE> Result;
-  };
-};
-
-// testing shit
-
-struct Base
-{
-  void baseGo()
-  {
-    cout << "Hello from Base" << endl;
-  }
-};
-
 // this level of indirection to the Specialize class 
 // is need by VC6 to avoid error C2510
 template<class POLICY, class ACTUAL, class BASE>
@@ -139,6 +48,7 @@ struct FriendlySpec
 {
   typedef Specialize<typename POLICY::Identity>::Params<typename POLICY::Params, ACTUAL, BASE>::Result Result;
 };
+
 
 template<class ACTUAL, class TYPE_LIST>
 struct PolicyHelper;
@@ -181,6 +91,87 @@ struct PolicyHelper : PolicyHelpImpl<TYPE_LIST>::Actual<ACTUAL>::Policy
   }
 };
 
+/////////////////////////////////////////////////////////////////////////////
+// Testing Stuff
+
+template<class ACTUAL, class BASE>
+struct UpperPolicyImpl : public BASE
+{
+  void upperGo()
+  {
+    ACTUAL * t = (ACTUAL *) this;
+    t->UpperMethod();
+    t->LowerType::shadow();
+  }
+  
+  void shadow()
+  {
+    cout << "Upper Shadow" << endl;
+  }
+  
+  void crawl()
+  {
+    typedef BASE Parent;
+    cout << __FILE__ << ":" << __LINE__ << " " << typeid(Parent).name() << endl;
+    Parent::crawl();
+  }  
+};
+
+struct UpperPolicy
+{
+  typedef UpperPolicy Identity;
+  typedef EmptyType Params;
+};
+
+template<>
+struct Specialize<UpperPolicy>
+{
+  template<class PARAMS, class ACTUAL, class BASE>
+  struct Params
+  {
+    typedef UpperPolicyImpl<ACTUAL, BASE> Result;
+  };
+};
+
+template<class ACTUAL, class BASE>
+struct LowerPolicyImpl : public BASE
+{
+  void lowerGo()
+  {
+    ACTUAL * t = (ACTUAL *) this;
+    cout << "Lower: My BASE is " << typeid(BASE).name() << endl;
+    t->LowerMethod();
+  }
+  
+  void shadow()
+  {
+    cout << "Lower Shadow" << endl;
+  }
+  
+  void crawl56()
+  {
+    typedef BASE Parent;
+    cout << __FILE__ << ":" << __LINE__ << " " << typeid(Parent).name() << endl;
+    Parent::crawl();
+  }
+};
+
+struct LowerPolicy
+{
+  typedef LowerPolicy Identity;
+  typedef EmptyType Params;
+};
+
+template<>
+struct Specialize<LowerPolicy>
+{
+  template<class PARAMS, class ACTUAL, class BASE>
+  struct Params
+  {
+    typedef LowerPolicyImpl<ACTUAL, BASE> Result;
+  };
+};
+
 template<class UPPER_POLICY, class LOWER_POLICY>
 struct PolicyClass : 
   public 
@@ -218,6 +209,8 @@ struct PolicyClass :
   };
 };
 
+
+
 int main()
 {
   typedef PolicyClass<UpperPolicy, LowerPolicy> P;
@@ -235,7 +228,7 @@ int main()
   
   p.upperGo();
   p.lowerGo();
-  
+
 /*
   typedef Specialize<Policy>::Params<NullType, Base1>::Result PolicyBase1;
   typedef Specialize<Policy>::Params<NullType, Base2>::Result PolicyBase2;  
@@ -250,3 +243,6 @@ int main()
   
   return 0;
 }
+
+
+
