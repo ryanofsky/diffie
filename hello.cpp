4a70901b08dde68b5af59e578824a07cb88e8fd2
Russell Yanofsky <russ@yanofsky.org>
2002-06-26 01:54:58 +0000

    - added simple check to improve append peformance when the list being appended   is empty. - began writing support for Policy chaining so policies can be composed of other   policies without using multiple inheritance.

--- hello.cpp
+++ hello.cpp
@@ -27,7 +27,7 @@ class TopType
 };
 
 
-// Function:     AppendImpl(LIST1, LIST2)
+// Function:     Append(LIST1, LIST2)
 //
 // Purpose:      Appends LIST2 to LIST1 by returning a copy of LIST1 with its 
 //               null terminator replaced by LIST2
@@ -35,23 +35,40 @@ class TopType
 // Requirements: LIST1 and LIST2 are typelists (although either or both can be
 //               null)
 //
+// Pseudocode:   if (LIST2 == NullType) // optimization
+//                 return LIST1;
+//               else 
+//                 return Append1(LIST1, LIST2);
+
+// Function:     Append1(LIST1, LIST2)
 // Pseudocode:   if (LIST1 != NullType)
-//                 return List<LIST1::Head, AppendImpl(LIST1::Tail, LIST2)
+//                 return List<LIST1::Head, Append1(LIST1::Tail, LIST2)
 //               else
 //                 return LIST2;
+//
+//              The first if (branch) isn't needed for correctness, it
+//              just obviates the need for a list traversal when
+//              there is nothing to append.
+//
+//              Run time is proportional to the size of the first
+//              list, except in the special case where the the
+//              second list is empty, when the runing time will
+//              be constant. Generally, it is more efficient
+//              to append a long list to a short list than
+//              a short list to a long list.
 
 template<class LIST1>
-struct AppendImpl
+struct AppendImpl2
 {
   template<class LIST2>
   struct apply
   {
-    typedef List<LIST1::Head, AppendImpl<LIST1::Tail>::apply<LIST2>::type > type;
+    typedef List<LIST1::Head, AppendImpl2<LIST1::Tail>::apply<LIST2>::type > type;
   };
 };
 
 template<>
-struct AppendImpl<NullType>
+struct AppendImpl2<NullType>
 {
   template<class LIST2>
   struct apply
@@ -60,13 +77,34 @@ struct AppendImpl<NullType>
   };
 };
 
+template<class LIST2>
+struct AppendImpl1
+{
+  template<class LIST1>
+  struct apply
+  {
+    typedef AppendImpl2<LIST1>::apply<LIST2>::type type;
+  };
+};
+
+template<>
+struct AppendImpl1<NullType>
+{
+  template<class LIST1>
+  struct apply
+  {
+    typedef LIST1 type;
+  };
+};
+
+
 // Append the LIST2 to LIST1.
 // Procedure: 
 
 template<class LIST1, class LIST2>
 struct Append
 {
-  typedef AppendImpl<LIST1>::apply<LIST2>::type type;
+  typedef AppendImpl1<LIST1>::apply<LIST2>::type type;
 };
 
 template<class HEAD, class TAIL>
@@ -83,7 +121,71 @@ struct GetPolicy2
   typedef typename POLICY::template Policy<ACTUAL, BASE> type;
 };
 
+// Function:     PolicyChainImpl<TYPE_LIST, ACTUAL>
+// Purpose:
+// Requirements: TYPE_LIST is a typelist (possibly NULL), ACTUAL
+//               should be the type of the policy class which
+//               is at the very bottom of the inheritance chain.
+//               the value of ACTUAL is ignored by this function,
+//               it is only passed as an argument each policy class
+//               on the chain. The TYPE_LIST must consist of policy
+//               classes only. All policy classes will have inner
+//               templated classes that look like:
+//                 
+//                 template<class ACTUAL, class BASE>
+//                 struct Policy : public BASE
+//                 {
+//                   // policies' methods, typedefs, and members
+//                   // defined here...
+//                 };
+
+//////////////////////// Revision 1, No Chaining or Custom Inheritance
 //
+// Pseudocode:
+// if (TYPE_LIST == NullType)
+// {
+//   NextList = PolicyChainImpl<TYPE_LIST::Tail, ACTUAL>;
+//   Policy = TYPE_LIST::Head::apply<ACTUAL, Next::Head>;
+//   return List<Policy, NextList>;
+// }
+// else
+//   return List<TopClass, NullType>;
+//
+
+
+// Notes:
+//
+// Purpose of chaining: Allow policies to be themselves composed of policies
+//   Chained policies should not be on the returned inheritance list
+
+template<class ACTUAL, class POLICIES>
+struct ChainData
+{
+  typedef ACTUAL Actual;
+  typedef POLICIES Policies;
+};
+
+template<class CHAIN, class EMBEDDED_CHAIN>
+struct PolicyChainReturn
+{
+  typedef CHAIN_DATA Chain;
+  typedef EMBEDDED_CHAIN_DATA EmbeddedChain;
+};
+
+
+
+template<class TYPE_LIST>
+struct PolicyChainImpl
+{
+  template<class ACTUAL>
+  struct apply
+  {
+    typedef type;
+  };
+}
+
+template<>
+struct PolicyChainImpl<NullType>
 
 template<class TYPE_LIST>
 struct PolicyChainImpl
@@ -111,7 +213,7 @@ template<class ACTUAL, class TYPE_LIST>
 struct PolicyChain
 {
   typedef PolicyChainImpl<TYPE_LIST>::apply<ACTUAL> Helper;
-  typedef typename Helper::Policy Policy;
+  typedef typename Helper::Policy type;
   typedef Helper::Next Next;
 };
 
