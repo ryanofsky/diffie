401d46bb450ab8e7ce7cb52496de23e8a2afd136
Russell Yanofsky <russ@yanofsky.org>
2002-07-21 03:20:50 +0000

    - new testing for chaining

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-#include <typeinfo>
+using namespace std;
 
 #include "general.hpp"
 #include "policy.hpp"
-
-using namespace std;
 
 struct P1
 {
@@ -23,29 +21,98 @@ struct P1
   };                                           
 };
 
-struct P2
+struct Stupid
+{
+  void stupidGo()
+  {
+    cout << "Helo from stupid" << endl;
+  }
+};
+
+struct Dumb
+{
+  void dumbGo()
+  {
+    cout << "Helo from dumb" << endl;
+  }
+};
+
+struct Slow
+{
+  void slowGo()
+  {
+    cout << "Helo from slow" << endl;
+  }
+};
+
+struct P4
 {
   typedef NullType Policies;
+  typedef List<Slow, List<Dumb, List<Stupid, NullType> > > Inherits;
+
+  template<class ACTUAL, class BASE, class CHAIN>           
+  struct Policy : public BASE                  
+  {
+    void p4Go()
+    {
+      ACTUAL * t = (ACTUAL *) this;
+      t->print("Hello from P4");
+      //t->topGo();
+      
+      cout << "TOP: Here's the class that was inherited "
+        << typeid(InheritFrom<Inherits>::type).name() << endl;        
+      
+      //t->Dumb::dumbGo();
+    };                                             
+  };                                           
+};
+
+struct P3
+{
+  typedef List<P4, NullType> Policies;
+  typedef NullType Inherits;
+  
+  template<class ACTUAL, class BASE, class CHAIN>           
+  struct Policy : public BASE                  
+  {
+    typedef CHAIN::Head p4;
+    void p3Go()
+    {
+      ACTUAL * t = (ACTUAL *) this;
+      t->print("Hello from P3");
+      t->p4::p4Go();
+    };                                             
+  };                                           
+};
+
+struct P2
+{
+  typedef List<P3, NullType> Policies;
   typedef NullType Inherits;
 
   template<class ACTUAL, class BASE, class CHAIN>           
   struct Policy : public BASE                  
   {
+    typedef CHAIN::Head p3;
     void p2Go()
     {
       ACTUAL * t = (ACTUAL *) this;
       t->print("Hello from P2");
+      t->p3::p3Go();
     };                                             
   };                                           
 };
 
+#define CALL_PolicyChain(POLICIES, ACTUAL) PolicyChainImpl<POLICIES>::template apply<ACTUAL, NullType, NullType>
+
 template<class ACTUAL, class POLICIES>
-struct PolicyHelper : PolicyChainImpl<POLICIES>::apply<ACTUAL, NullType, NullType>::Chain::Head
+struct PolicyHelper 
+  : public TGET_Head(TGET_Chain(CALL_PolicyChain(POLICIES, ACTUAL)))
 {
-  typedef PolicyChainImpl<POLICIES>::apply<ACTUAL, NullType, NullType>::Chain Chain;
+  typedef TGET_Chain(CALL_PolicyChain(POLICIES, ACTUAL)) Chain;
 };
 
-struct Bottom : PolicyHelper<Bottom, List< P1, List<P2, NullType> > >
+struct Bottom : public PolicyHelper<Bottom, List< P1, List<P2, NullType> > >
 {
   typedef Chain::Head c1;
   typedef Chain::Tail::Head c2;
