9df597202750e752463e3b707dc69b3e4a684436
Russell Yanofsky <russ@yanofsky.org>
2002-07-23 02:32:29 +0000

    - renamed from general.hpp

---
+++ typelist.hpp
@@ -0,0 +1,18 @@
+#ifndef general_hpp
+#define general_hpp
+
+#include <boost/mpl/list.hpp>
+
+struct EmptyType{};
+
+typedef boost::mpl::null_node NullType;
+
+template< typename HEAD, typename TAIL > 
+struct List : public boost::mpl::list_node
+  < typename BOOST_MPL_AUX_NEXT(TAIL::size)
+  , HEAD
+  , TAIL
+  > 
+{};
+
+#endif 
