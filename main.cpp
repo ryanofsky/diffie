d1c34c920fc7d18e0da805494f3e16eaa6744578
Russell Yanofsky <russ@yanofsky.org>
2002-07-24 04:53:16 +0000

    - empty main function

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#pragma warning(disable:4786)
+#pragma warning(disable:4503)
+
+#include <typeinfo>
+#include <iostream>
+using namespace std;
+
+#include "typelist.hpp"
+#include "policy.hpp"
+#include "dhaes.hpp"
+
+int main()
+{
+  return 0;
+}
