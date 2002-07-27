8933704e5403bec2e162ce7e6f734a740dedcced
Russell Yanofsky <russ@yanofsky.org>
2002-07-27 01:01:50 +0000

    - just changes in whitespace

--- typelist.hpp
+++ typelist.hpp
@@ -7,7 +7,7 @@ struct EmptyType{};
 
 typedef boost::mpl::null_node NullType;
 
-template< typename HEAD, typename TAIL > 
+template<typename HEAD, typename TAIL> 
 struct List : public boost::mpl::list_node
   < typename BOOST_MPL_AUX_NEXT(TAIL::size)
   , HEAD
